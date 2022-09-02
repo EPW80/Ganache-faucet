ff093e0c63f788dc563a4dbae2da48670c8a3672
EWP80 <erikpw009@gmail.com>
2022-09-01 20:45:23 -0700

    added css styling to app

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -3,5 +3,26 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
    
+    address[] public funders;
+
+    receive() external payable {}
+
+    function addFunds() external payable {
+        funders.push(msg.sender);
+    }
+
+    function getAllFunders() public view returns (address[] memory) {
+        return funders;
+    }
+
+    function getFunderAtIndex(uint8 index) external view returns(address) {
+        address[] memory _funders = getAllFunders();
+        return _funders[index];
+
+    }
+
+    // const instance = await Faucet.deployed()
+    // instance.addFunds({from: accounts[0], value: "2})
+
 
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "bulma/css/bulma.min.css";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
