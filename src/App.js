8bd5dcf3deb7a41b7fafdebe5f1d0d85a2ede058
EWP80 <erikpw009@gmail.com>
2022-09-10 20:40:55 -0700

    metamask credentials deployed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import Web3 from "web3";
+import detectEthereumProvider from '@metamask/detect-provider'
+
 function App() {
   const [web3Api, setWeb3Api] = useState({
     provider: null,
@@ -11,28 +13,20 @@ function App() {
 
   useEffect(() => {
     const loadProvider = async () => {
-      let provider = null;
-      if (window.ethereum) {
-        provider = window.ethereum;
-        
-        try {
-          await provider.request({method: "eth_requestAccounts"});
-        } catch {
-          console.error("User denied accounts access!")
-        }
+      const provider = await detectEthereumProvider()
 
+      if (provider) {
+        setWeb3Api({
+          web3: new Web3(provider),
+          provider
+        })
+      } else {
+        console.error("Please install Metamask. ")
       }
-      else if (window.web3) {
-        provider = window.web3.currentProvider
-      }
-      else if (!process.env.production) {
-        provider = new Web3.providers.HttpProvider("http://localhost:7545")
-      }
-      setWeb3Api({
-        web3: new Web3(provider),
-        provider
-      })
+
+
     }
+
     loadProvider()
   }, [])
 
@@ -54,7 +48,7 @@ function App() {
             <strong>Account: </strong>
           </span>
           <h1>
-            { account ? account : "not connected" }
+            {account ? account : "not connected"}
           </h1>
           <div className="balance-view is-size-2">
             Current Balance: <strong>1</strong> ETH
@@ -66,4 +60,5 @@ function App() {
     </>
   );
 }
+
 export default App;
