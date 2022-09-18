cda519f548537c236109a4f03b165e395add5ee7
EWP80 <erikpw009@gmail.com>
2022-09-18 00:48:50 -0700

    refactored App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "./App.css";
 import Web3 from "web3";
 import detectEthereumProvider from '@metamask/detect-provider'
@@ -11,7 +11,9 @@ function App() {
     contract: null
   })
 
+  const [balance, setBallance] = useState(null)
   const [account, setAccount] = useState(null)
+  const [shouldReload, reload] = useState(false)
 
   useEffect(() => {
     const loadProvider = async () => {
@@ -33,6 +35,16 @@ function App() {
   }, [])
 
   useEffect(() => {
+    const loadBalance = async () => {
+      const { contract, web3 } = web3Api
+      const balance = await web3.eth.getBalance(contract.address)
+      setBallance(web3.utils.fromwei(balance, "ether"))
+    }
+
+    web3Api.contract && loadBalance()
+  }, [web3Api, shouldReload])
+
+  useEffect(() => {
     const getAccount = async () => {
       const accounts = await web3Api.web3.eth.getAccounts()
       setAccount(accounts[0])
@@ -40,6 +52,17 @@ function App() {
 
     web3Api.web3 && getAccount()
   }, [web3Api.web3])
+
+  const addFunds = useCallback(async () => {
+    const { contract, web3 } = web3Api
+    await contract.addFunds({
+      from: account,
+      value: web3.utils.toWei("1", "ether",)
+    })
+
+
+    reloadEffect()
+  }, [web3Api, account])
 
   return (
     <>
@@ -49,27 +72,30 @@ function App() {
             <span>
               <strong className="mr-2">Account: </strong>
             </span>
-            { account ?
+            {account ?
               <div>{account}</div> :
               <button
                 className="button is-small is-rounded"
                 onClick={() =>
-                  web3Api.provider.request({method: "eth_requestAccounts"}
+                  web3Api.provider.request({ method: "eth_requestAccounts" }
                   )}
 
               >
-                Connect Metamask Wallet
+                Connect Wallet
 
               </button>
 
             }
           </div>
           <div className="balance-view is-size-2 mb-4">
-            Current Balance: <strong>10</strong> ETH
+            Current Balance: <strong>1{balance}</strong> ETH
           </div>
           <button
-            className="button is-primary is-light is-rounded mr-2">Donate</button>
+            className="button is-primary is-light is-rounded mr-2">
+              Donate 0.1 eth
+              </button>
           <button
+          onClick={withdraw}
             className="button is-link is-light is-rounded">Withdraw</button>
         </div>
       </div>
--- src/utils/load-contract.js
+++ src/utils/load-contract.js
@@ -1,13 +1,13 @@
 import contract from "@truffle/contract"
 
 export const loadContract = async (name, provider) => {
-    const res = await fetch(`/contracts/${name}.json`)
-    const Artifact = await res.json()
+  const res = await fetch(`/contracts/${name}.json`)
+  const Artifact = await res.json()
 
-    const _contract = contract(Artifact)
-    _contract.setProvider(provider)
+  const _contract = contract(Artifact)
+  _contract.setProvider(provider)
 
-    const deployedContract = await _contract.deployed()
+  const deployedContract = await _contract.deployed()
 
-    return deployedContract
+  return deployedContract
 }
